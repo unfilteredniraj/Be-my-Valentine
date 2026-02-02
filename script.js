597b2585f33238031ecf7a61d694c42fa409c369
unfilteredniraj <unfilteredniraj@gmail.com>
2026-02-02 20:32:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-const noBtn = document.getElementById("noBtn");
-const card = document.querySelector(".card");
+const noBtn = document.getElementById("no");
+const area = document.querySelector(".button-area");
 
-noBtn.addEventListener("mouseover", () => {
-  const cardRect = card.getBoundingClientRect();
+noBtn.addEventListener("mouseenter", () => {
+  const areaRect = area.getBoundingClientRect();
   const btnRect = noBtn.getBoundingClientRect();
 
-  const maxX = cardRect.width - btnRect.width;
-  const maxY = cardRect.height - btnRect.height;
+  const maxX = areaRect.width - btnRect.width;
+  const maxY = areaRect.height - btnRect.height;
 
   const randomX = Math.random() * maxX;
   const randomY = Math.random() * maxY;
 
-  noBtn.style.left = `${randomX}px`;
-  noBtn.style.top = `${randomY}px`;
-});
-
-document.getElementById("yesBtn").addEventListener("click", () => {
-  alert("Yay! 💖 See you on Valentine’s Day!");
+  noBtn.style.left = ${randomX}px;
+  noBtn.style.top = ${randomY}px;
 });
